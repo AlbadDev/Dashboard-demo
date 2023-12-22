f01a3ed7342de6f9f663e86aec92a548cd8cda7d
albaddev <alioubkeita95@gmail.com>
2023-12-22 17:31:59 +0000

    New: app setup

---
+++ app/dashboard.js
@@ -0,0 +1,374 @@
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/eMRcBtKobbZ
+ */
+'use client'
+import Link from "next/link"
+import { ResponsiveLine } from "@nivo/line"
+import { ResponsiveBar } from "@nivo/bar"
+import RadialChart from "./radialChart"
+
+export const Dashboard = () => {
+
+
+
+
+
+
+
+  return (
+    <div className="flex h-screen bg-gray-200">
+      <aside className="bg-white w-64 p-4 shadow-md">
+        <nav className="space-y-4">
+          <Link className="flex items-center gap-2 text-lg font-semibold" href="#">
+            <HomeIcon className="w-6 h-6" />
+            <span>Home</span>
+          </Link>
+          <Link className="flex items-center gap-2 text-lg font-semibold" href="#">
+            <UserIcon className="w-6 h-6" />
+            <span>Users</span>
+          </Link>
+          <Link className="flex items-center gap-2 text-lg font-semibold" href="#">
+            <SettingsIcon className="w-6 h-6" />
+            <span>Settings</span>
+          </Link>
+          <Link className="flex items-center gap-2 text-lg font-semibold" href="#">
+            <BarChartIcon className="w-6 h-6" />
+            <span>Reports</span>
+          </Link>
+        </nav>
+      </aside>
+      <main className="flex flex-col flex-1 overflow-auto p-4">
+        <header className="flex items-center justify-between mb-6">
+          <h1 className="text-2xl font-semibold">Dashboard</h1>
+          <div className="flex items-center">
+            <button className="mr-4" variant="outline">
+              Settings
+            </button>
+            <avatar className="w-10 h-10" src="/placeholder.svg?height=40&width=40" />
+          </div>
+        </header>
+        <section className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+          <div className="bg-white shadow rounded p-4">
+            <h2 className="text-lg font-semibold mb-2">Total Users</h2>
+            <p className="text-lg font-semibold text-gray-500">1234</p>
+          </div>
+          <div className="bg-white shadow rounded p-4">
+            <h2 className="text-lg font-semibold mb-2">New Signups</h2>
+            <p className="text-lg font-semibold text-gray-500">56</p>
+          </div>
+          <div className="bg-white shadow rounded p-4">
+            <h2 className="text-lg font-semibold mb-2">Revenue</h2>
+            <p className="text-lg font-semibold text-gray-500">$12000</p>
+          </div>
+          <div className="bg-white shadow rounded p-4">
+            <h2 className="text-lg font-semibold mb-2">Active Users</h2>
+            <p className="text-lg font-semibold text-gray-500">789</p>
+          </div>
+        </section>
+        <section className="grid gap-4 mt-8 md:grid-cols-2">
+          <div className="bg-white shadow rounded p-4">
+            <h2 className="text-lg font-semibold mb-2">User Acquisition</h2>
+            <div className="pt-4">
+              <CurvedlineChart className="w-full aspect-[4/3]" />
+            </div>
+          </div>
+          <div className="bg-white shadow rounded p-4">
+            <h2 className="text-lg font-semibold mb-2">Revenue Growth</h2>
+            <div className="pt-4">
+              <BarChart className="w-full aspect-[4/3]" />
+            </div>
+          </div>
+
+          <div className="bg-white shadow rounded p-4">
+            <h2 className="text-lg font-semibold mb-2">Uses of Cloud Computing</h2>
+            <div className="pt-4">
+              <RadialChart className="w-full aspect-[4/3]" />
+            </div>
+          </div>
+          {/* RadialChart */}
+        </section>
+      </main>
+    </div>
+  )
+}
+export default Dashboard
+function BarChart(props) {
+  return (
+    <div {...props}>
+      <ResponsiveBar
+        data={[
+          {
+            name: "A",
+            data: 111,
+          },
+          {
+            name: "B",
+            data: 157,
+          },
+          {
+            name: "C",
+            data: 129,
+          },
+          {
+            name: "D",
+            data: 187,
+          },
+          {
+            name: "E",
+            data: 119,
+          },
+          {
+            name: "F",
+            data: 22,
+          },
+          {
+            name: "G",
+            data: 101,
+          },
+          {
+            name: "H",
+            data: 83,
+          },
+        ]}
+        keys={["data"]}
+        indexBy="name"
+        margin={{ top: 50, right: 50, bottom: 50, left: 60 }}
+        padding={0.3}
+        valueScale={{ type: "linear" }}
+        indexScale={{ type: "band", round: true }}
+        colors={{ scheme: "paired" }}
+        borderWidth={1}
+        borderColor={{
+          from: "color",
+          modifiers: [["darker", 0.2]],
+        }}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "Name",
+          legendPosition: "middle",
+          legendOffset: 45,
+          truncateTickAt: 0,
+        }}
+        axisLeft={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "Number",
+          legendPosition: "middle",
+          legendOffset: -45,
+          truncateTickAt: 0,
+        }}
+        theme={{
+          tooltip: {
+            container: {
+              fontSize: "12px",
+            },
+          },
+        }}
+        labelSkipWidth={12}
+        labelSkipHeight={12}
+        role="application"
+        ariaLabel="A bar chart showing data"
+      />
+    </div>
+  )
+}
+
+
+function BarChartIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="12" x2="12" y1="20" y2="10" />
+      <line x1="18" x2="18" y1="20" y2="4" />
+      <line x1="6" x2="6" y1="20" y2="16" />
+    </svg>
+  )
+}
+
+
+function CurvedlineChart(props) {
+  return (
+    <div {...props}>
+      <ResponsiveLine
+        data={[
+          {
+            id: "B",
+            data: [
+              { x: "2018-01-01", y: 7 },
+              { x: "2018-01-02", y: 5 },
+              { x: "2018-01-03", y: 11 },
+              { x: "2018-01-04", y: 9 },
+              { x: "2018-01-05", y: 12 },
+              { x: "2018-01-06", y: 16 },
+              { x: "2018-01-07", y: 13 },
+              { x: "2018-01-08", y: 13 },
+            ],
+          },
+          {
+            id: "A",
+            data: [
+              { x: "2018-01-01", y: 9 },
+              { x: "2018-01-02", y: 8 },
+              { x: "2018-01-03", y: 13 },
+              { x: "2018-01-04", y: 6 },
+              { x: "2018-01-05", y: 8 },
+              { x: "2018-01-06", y: 14 },
+              { x: "2018-01-07", y: 11 },
+              { x: "2018-01-08", y: 12 },
+            ],
+          },
+        ]}
+        enableCrosshair={false}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        xScale={{
+          type: "time",
+          format: "%Y-%m-%d",
+          useUTC: false,
+          precision: "day",
+        }}
+        xFormat="time:%Y-%m-%d"
+        yScale={{
+          type: "linear",
+          min: 0,
+          max: "auto",
+        }}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "X",
+          legendOffset: 45,
+          legendPosition: "middle",
+          format: "%b %d",
+          tickValues: "every 1 day",
+        }}
+        axisLeft={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "Y",
+          legendOffset: -45,
+          legendPosition: "middle",
+        }}
+        colors={{ scheme: "paired" }}
+        pointSize={5}
+        pointColor={{
+          from: "color",
+          modifiers: [["darker", 0.2]],
+        }}
+        pointBorderWidth={2}
+        pointBorderColor={{
+          from: "color",
+          modifiers: [["darker", 0.2]],
+        }}
+        pointLabelYOffset={-12}
+        useMesh={true}
+        curve="monotoneX"
+        legends={[
+          {
+            anchor: "bottom-right",
+            direction: "column",
+            justify: false,
+            translateX: 100,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: "left-to-right",
+            itemWidth: 80,
+            itemHeight: 20,
+            symbolSize: 14,
+            symbolShape: "circle",
+          },
+        ]}
+        theme={{
+          tooltip: {
+            container: {
+              fontSize: "12px",
+            },
+          },
+        }}
+        role="application"
+      />
+    </div>
+  )
+}
+
+
+function HomeIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+      <polyline points="9 22 9 12 15 12 15 22" />
+    </svg>
+  )
+}
+
+
+function SettingsIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
+      <circle cx="12" cy="12" r="3" />
+    </svg>
+  )
+}
+
+
+function UserIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+      <circle cx="12" cy="7" r="4" />
+    </svg>
+  )
+}
---
+++ app/radialChart.js
@@ -0,0 +1,105 @@
+import './globals.css';
+import '@grapecity/wijmo.styles/wijmo.css';
+import {useState} from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { RadialGauge, Range } from '@grapecity/wijmo.react.gauge';
+import { InputNumber } from '@grapecity/wijmo.react.input';
+
+const RadialChart = () => {
+  const [state, setState] = useState({
+    min: 0,
+    max: 100,
+    step: 5,
+    value: 75,
+    startAngle: 0,
+    sweepAngle: 180,
+    autoScale: true,
+  });
+
+  return (
+    <div className="container mx-auto p-4">
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <div className="form-group">
+            <label htmlFor="value" className='labelInput'>Value:</label>
+            <InputNumber
+              id="value"
+              min={state.min}
+              max={state.max}
+              step={state.step}
+              value={state.value}
+              valueChanged={(s) => setState((prev) => ({ ...prev, value: s.value }))}
+              className='inputControl'
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="startAngle">Start Angle:</label>
+            <InputNumber
+              id="startAngle"
+              step={10}
+              min={-360}
+              max={360}
+              value={state.startAngle}
+              valueChanged={(s) => setState((prev) => ({ ...prev, startAngle: s.value }))}
+              className='inputControl'
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="sweepAngle">Sweep Angle:</label>
+            <InputNumber
+              id="sweepAngle"
+              step={10}
+              min={-360}
+              max={360}
+              value={state.sweepAngle}
+              valueChanged={(s) => setState((prev) => ({ ...prev, sweepAngle: s.value }))}
+              className='inputControl'
+            />
+          </div>
+
+          <div className="form-check">
+            <label htmlFor="autoScale" className="form-check-label">
+              Auto Scale:
+            </label>
+            <input
+              id="autoScale"
+              type="checkbox"
+              className="form-check-input"
+              checked={state.autoScale}
+              onChange={(e) => setState((prev) => ({ ...prev, autoScale: e.target.checked }))}
+            //   id='inputControl'
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="gauge-holder">
+            <RadialGauge
+              isReadOnly={false}
+              min={state.min}
+              max={state.max}
+              step={state.step}
+              value={state.value}
+              showRanges={false}
+              startAngle={state.startAngle}
+              sweepAngle={state.sweepAngle}
+              autoScale={state.autoScale}
+              valueChanged={(s) => setState((prev) => ({ ...prev, value: s.value }))}
+            >
+              <Range min={0} max={33} color="red" />
+              <Range min={33} max={66} color="yellow" />
+              <Range min={66} max={100} color="green" />
+            </RadialGauge>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RadialChart
+
+
+
